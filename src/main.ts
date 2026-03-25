9f9d44b962a6c3122feae99cd4e33a8c76bf67e7
WeiJinqi <changshanshi@outlook.com>
2026-03-25 18:19:26 +0000

    feature: add transactionFilter to modify headings dynamiclly

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,48 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin } from "obsidian";
+import { EditorState, Transaction, ChangeSpec } from "@codemirror/state";
 
-// Remember to rename these classes and interfaces!
+const autoHeadingAliasFilter = EditorState.transactionFilter.of(
+    (tr: Transaction) => {
+        // return early if no changes
+        if (!tr.docChanged) return tr;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        let hasMatch = false;
+        const newChanges: ChangeSpec[] = [];
 
-	async onload() {
-		await this.loadSettings();
+        // iterate through all changes in the transaction
+        tr.changes.iterChanges((fromA, toA, fromB, toB, inserted) => {
+            const text = inserted.toString();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+            // match format [[filename#heading]]
+            const match = text.match(/^\[\[([^|\]]+)#([^|\]]+)\]\]$/);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+            if (match) {
+                hasMatch = true;
+                const filename = match[1];
+                const heading = match[2];
+                // replace with [[filename#heading|heading]]
+                const replacement = `[[${filename}#${heading}|${heading}]]`;
+                newChanges.push({
+                    from: fromA,
+                    to: toA,
+                    insert: replacement,
+                });
+            } else {
+                newChanges.push({
+                    from: fromA,
+                    to: toA,
+                    insert: inserted,
+                });
+            }
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        if (hasMatch) return { changes: newChanges };
+        return tr;
+    },
+);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-	}
-
-	onunload() {
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+export default class AutoHeadingLinkAliasPlugin extends Plugin {
+    async onload() {
+        this.registerEditorExtension(autoHeadingAliasFilter);
+    }
 }
